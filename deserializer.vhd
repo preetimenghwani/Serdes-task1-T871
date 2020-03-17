ef84d1284c917bdca68f68343586bf23ae24c669
preetimenghwani <61456975+preetimenghwani@users.noreply.github.com>
2020-03-18 01:12:30 +0530

    Update deserializer.vhd

--- deserializer.vhd
+++ deserializer.vhd
@@ -22,12 +22,12 @@ use IEEE.STD_LOGIC_1164.ALL;
 use ieee.std_logic_unsigned.all;
 
 entity deserializer is
-	port( clk				:	in std_logic;
+	port(           clk				:	in std_logic;
 			reset				:	in std_logic;
 			DIN2				:	in std_logic;
 			DOUT2				:	out std_logic_vector(7 downto 0):=(others=>'0');
-			clk_out1       :  out std_logic;
-			link_trained   :  out std_logic:='0'
+			clk_out1                        :       out std_logic;
+			link_trained                    :       out std_logic:='0'
 	    );
 end deserializer;
 
@@ -37,11 +37,11 @@ architecture Behavioral of deserializer is
 
 	signal counter		:	std_logic_vector(2 downto 0);
 	signal sel    		:	std_logic_vector(2 downto 0);
-	signal DOUT    	:  std_logic_vector(7 downto 0) := (others => '0');
-	signal clk_out	   :  std_logic:='0';
-	signal sync_rst   :  std_logic:='0';
-	signal linked     :  std_logic:='0';
-	signal counter2   :  std_logic_vector(2 downto 0):=(others=>'0');
+	signal DOUT      	:  std_logic_vector(7 downto 0) := (others => '0');
+	signal clk_out	        :  std_logic:='0';
+	signal sync_rst         :  std_logic:='0';
+	signal linked           :  std_logic:='0';
+	signal counter2         :  std_logic_vector(2 downto 0):=(others=>'0');
 	
 begin
 	
@@ -51,10 +51,10 @@ link_trained<=linked;
 bitslip_proc : process(clk) 
 	begin
     if rising_edge(clk) then
-		if sync_rst='1' then
+	if sync_rst='1' then
 			counter<="000";
 			linked <= '0';
-	 else if Dout2(7 downto 0)=testpattern and counter="001" and linked='0' and counter2="010"then
+        else if Dout2(7 downto 0)=testpattern and counter="001" and linked='0' and counter2="010"then
 		linked<='1';
 		counter<="001";  --syncing counter
 	else if Dout2(7 downto 0) /= testpattern and counter="011" and counter2="100" then
@@ -64,7 +64,7 @@ bitslip_proc : process(clk)
 	end if;
 	end if;
 	end if;
-		end if;
+   end if;
 	end process;
 	
 	newcounter_proc: process(counter,clk);
@@ -72,7 +72,7 @@ bitslip_proc : process(clk)
 			if rising_edge(clk) then
 				if(counter='8')
 					counter2=counter2+1;
-		      end if;
+		                end if;
 			end if;
 	 end process;
 				
