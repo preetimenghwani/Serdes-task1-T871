2d4d56d7feb81d9e5ada9955aa932f0f169568dc
preetimenghwani <61456975+preetimenghwani@users.noreply.github.com>
2020-03-22 15:40:06 +0530

    Add files via upload

--- seri.vhd
+++ seri.vhd
@@ -20,38 +20,43 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE ieee.std_logic_unsigned.ALL;
+use ieee.numeric_std.all;
+
+
+
 ENTITY serealisercomb IS
-    PORT (
-        DIN1    : IN STD_LOGIC_VECTOR(11 DOWNTO 0);
-        clk     : IN STD_LOGIC;
-		  clk_out : in std_logic;
-        reset   : IN STD_LOGIC;
-		  depth_sel: integer;
-        DOUT1   : OUT STD_LOGIC;
-        ready   : in std_logic :='0'-- Link trained input from Deserializer
-        --sel2 : out std_logic_vector(2 downto 0)
+    port (
+        din_ser    : IN STD_LOGIC_VECTOR(11 DOWNTO 0):=(others=>'0'); --input to serializer
+        clk        : IN STD_LOGIC;                     --high speed input clocl
+		  clk_in     : in std_logic;                     --low speed input clock 
+        reset_ser  : IN STD_LOGIC;                     --reset signal for serializer
+		  depth_sel  : in std_logic_vector(1 downto 0):=(others=>'0');                          --for selecting bit depth depth_sel 4=> 8 bit ,2=> 10 bit, 0=> 12 bit                        
+        dout_ser   : OUT STD_LOGIC;                    --output of serializer
+        ready      : in std_logic :='0'                -- Link trained input from Deserializer
+        
     );
 END serealisercomb;
 ARCHITECTURE Behavioral OF serealisercomb IS
 
-    SIGNAL sel     : std_logic_vector(3 DOWNTO 0):=(others=>'0');
-    SIGNAL counter : std_logic_vector(3 DOWNTO 0):=(others=>'0');
+    SIGNAL sel        : std_logic_vector(3 DOWNTO 0):=(others=>'0');
+    SIGNAL counter    : std_logic_vector(3 DOWNTO 0):=(others=>'0');
     --SIGNAL clk_out : std_logic;
-    SIGNAL Din     : std_logic_vector(11 DOWNTO 0):=(others=>'0');
+    SIGNAL Din       : std_logic_vector(11 DOWNTO 0);
+	 signal bit_depth : integer;
     --signal linked_buf : std_logic := '0';
-    SIGNAL sync_rst : std_logic := '0';
+    --SIGNAL sync_rst : std_logic := '0';
     CONSTANT test_pattern : std_logic_vector (11 DOWNTO 0) := "101011001111";
 	 --signal depth_sel: integer;
+  
 
 BEGIN
-    sync_rst <= reset WHEN rising_edge(clk);
 
-    PROCESS (clk, reset)
+    PROCESS (clk, reset_ser)
     BEGIN
         if (rising_edge(clk)) THEN
-            if (sync_rst = '1') THEN
+            if (reset_ser = '1') THEN
                 counter <= (others => '0');
-			   elsif(counter=11-depth_sel) then
+			   elsif(counter="1011"-depth_sel) then
 					counter<= (others => '0');
             else 
                 counter <= counter + 1;
@@ -59,17 +64,17 @@ BEGIN
 			 end if;
 		
     END PROCESS;
- 
+	 
+bit_depth<= to_integer(unsigned(depth_sel));
     sel <= counter;
-    --clk_out <= counter(2);
-    p_clk_out : PROCESS (clk_out, reset)
+    p_clk_out : PROCESS (clk_in, reset_ser)
     BEGIN
-        IF (rising_edge(clk_out)) THEN
-            IF (sync_rst = '0') THEN
+        IF (rising_edge(clk_in)) THEN
+            IF (reset_ser = '0') THEN
 					if(ready='1') then
-						din(11 downto depth_sel) <= din1(11-depth_sel downto 0); 
+						din(11 downto bit_depth) <= din_ser(11-bit_depth downto 0); 
 					else 
-						din(11 downto depth_sel)<=test_pattern(11-depth_sel downto 0);
+						din(11 downto bit_depth)<=test_pattern(11-bit_depth downto 0);
 					end if;
             END IF;
         END IF;
@@ -79,20 +84,20 @@ BEGIN
     PROCESS (DIN, sel)
         BEGIN
             CASE sel IS
-                WHEN"0000" => DOUT1 <= DIN(11);
-                WHEN"0001" => DOUT1 <= DIN(10);
-                WHEN"0010" => DOUT1 <= DIN(9);
-                WHEN"0011" => DOUT1 <= DIN(8);
-                WHEN"0100" => DOUT1 <= DIN(7);
-                WHEN"0101" => DOUT1 <= DIN(6);
-                WHEN"0110" => DOUT1 <= DIN(5);
-                WHEN"0111" => DOUT1 <= DIN(4);
-                when"1000" => DOUT1 <= DIN(3);
-                WHEN"1001" => DOUT1 <= DIN(2);
-                WHEN"1010" => DOUT1 <= DIN(1);
-                WHEN"1011" => DOUT1 <= DIN(0);
+                WHEN"0000" => dout_ser <= DIN(11);
+                WHEN"0001" => dout_ser <= DIN(10);
+                WHEN"0010" => dout_ser <= DIN(9);
+                WHEN"0011" => dout_ser <= DIN(8);
+                WHEN"0100" => dout_ser <= DIN(7);
+                WHEN"0101" => dout_ser <= DIN(6);
+                WHEN"0110" => dout_ser <= DIN(5);
+                WHEN"0111" => dout_ser <= DIN(4);
+                when"1000" => dout_ser <= DIN(3);
+                WHEN"1001" => dout_ser <= DIN(2);
+                WHEN"1010" => dout_ser <= DIN(1);
+                WHEN"1011" => dout_ser <= DIN(0);
                 WHEN OTHERS => 
-                    DOUT1 <= 'Z';
+                    dout_ser <= 'Z';
             END CASE;
         END PROCESS;
  
