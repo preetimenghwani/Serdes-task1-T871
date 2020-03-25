8ee8208bf1900af239f6ac24ea3d50b4e2f0877a
preetimenghwani <61456975+preetimenghwani@users.noreply.github.com>
2020-03-25 20:01:40 +0530

    Update deser.vhd

--- deser.vhd
+++ deser.vhd
@@ -17,153 +17,146 @@
 -- Additional Comments: 
 --
 ----------------------------------------------------------------------------------
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
 USE ieee.std_logic_unsigned.ALL;
-use ieee.numeric_std.all;
+USE ieee.numeric_std.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+ENTITY deserializer IS
+	PORT (
+		clk_in_deser       : IN std_logic := '0';
+		reset_deser        : IN std_logic := '0';
+		din_deser          : IN std_logic := '0';
+		depth_sel          : IN std_logic_vector(2 DOWNTO 0) := (OTHERS => '0');
+		clk_out_deser      : OUT std_logic := '0';
+		link_trained       : OUT std_logic := '0';
+		dout_deser         : OUT std_logic_vector(11 DOWNTO 0) := (OTHERS => '0')
+		);
+	END deserializer;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+	ARCHITECTURE Behavioral OF deserializer IS
 
-entity deser is
-  port(
-         clk 	              : IN  std_logic:='0';
-         reset_deser         : IN  std_logic:='0';
-         DIN_deser 	        : IN  std_logic:='0';
-			depth_sel           : in std_logic_vector(1 downto 0):=(others=>'0');
-         clk_out             : out std_logic:='0';
-	      link_trained        : out std_logic:='0';
-			dout_deser          : out std_logic_vector(11 downto 0):=(others=>'0')
-			);
-end deser;
+		SIGNAL DOUT            : std_logic_vector(11 DOWNTO 0) := (OTHERS => '0');
+		SIGNAL DOUT2           : std_logic_vector(11 DOWNTO 0) := (OTHERS => '0');
+		SIGNAL counter         : std_logic_vector(3 DOWNTO 0) := (OTHERS => '0');
+		SIGNAL sel             : std_logic_vector(3 DOWNTO 0) := (OTHERS => '0');
+		SIGNAL counter_bit     : std_logic_vector(1 DOWNTO 0) := (OTHERS => '0');
+		SIGNAL linked          : std_logic := '0';
+		SIGNAL clk_out_sig     : std_logic := '0';
+		CONSTANT test_pattern  : std_logic_vector (11 DOWNTO 0) := "101110101111";
+		SIGNAL bit_depth       : INTEGER;
 
-architecture Behavioral of deser is
+	BEGIN
+		link_trained <= linked;
 
-signal DOUT           : std_logic_vector(11 downto 0):=(others=>'0');
-signal DOUT2          : std_logic_vector(11 downto 0):=(others=>'0');
-signal counter        :	std_logic_vector(3 downto 0):= (others => '0');
-signal sel            :	std_logic_vector(3 downto 0):= (others => '0');
-signal counter_bit    : std_logic_vector(1 downto 0):=(others=>'0');
-signal linked         : std_logic:='0';
-signal clk_in         : std_logic:='0';
-CONSTANT test_pattern : std_logic_vector (11 DOWNTO 0) := "101011001111";
-signal bit_depth: integer;
---signal dout_unclk     : std_logic_vector(11 downto 0):=(others=>'0');
---signal din_clked : std_logic:='0';
-begin
+		bitslip_proc : PROCESS (clk_in_deser)
+		BEGIN
+			IF rising_edge(clk_in_deser) THEN
 
---sync_rst <= reset when rising_edge(clk);
-link_trained<=linked;
+				IF reset_deser = '1' THEN
+					counter <= "0000";
+					linked <= '0';
+				ELSE
+					IF counter = 11 - bit_depth THEN
+						counter <= "0000";
+					ELSE
+						IF linked = '1' THEN
+							counter <= counter + 1;
+						ELSE
+							IF counter_bit = "01" AND counter = "0000" THEN
+								IF Dout2(11 - bit_depth DOWNTO 0) = test_pattern(11 - bit_depth DOWNTO 0) THEN
+									linked <= '1';
+ 
+								ELSE
+									linked <= linked;
+ 
+								END IF;
+								counter <= counter + '1';
+ 
+							ELSIF counter_bit = "11" AND counter = "0001" THEN
+								counter <= counter + 2; 
+ 
+							ELSE
+								counter <= counter + '1';
+							END IF;
+						END IF;
+					END IF;
+				END IF;
+			END IF;
+		END PROCESS;
+ 
+		newcounter_proc : PROCESS (counter, clk_in_deser)
+		BEGIN
+			IF rising_edge(clk_in_deser) THEN
+				IF (counter = "0111") THEN
+					counter_bit <= counter_bit + 1;
+				END IF;
+			END IF;
+		END PROCESS;
+ 
+		bit_depth <= to_integer(unsigned(depth_sel)); 
 
-bitslip_proc : process(clk) 
-	begin
-    if rising_edge(clk) then
+ 
+		sel <= counter;
+		PROCESS (clk_in_deser)
+			BEGIN
+				IF (rising_edge(clk_in_deser)) THEN
+					IF (reset_deser = '1') THEN
+						dout <= "000000000000";
+					ELSE
+						IF (sel = "0000") THEN
+							dout(11) <= din_deser;
+						ELSIF (sel = "0001") THEN
+							dout(10) <= (DIN_deser);
+						ELSIF (sel = "0010") THEN
+							dout(9) <= (DIN_deser);
+						ELSIF (sel = "0011") THEN
+							dout(8) <= din_deser;
+						ELSIF (sel = "0100") THEN
+							dout(7) <= din_deser;
+						ELSIF (sel = "0101") THEN
+							dout(6) <= din_deser;
+						ELSIF (sel = "0110") THEN
+							dout(5) <= din_deser;
+						ELSIF (sel = "0111") THEN
+							dout(4) <= din_deser;
+						ELSIF (sel = "1000") THEN
+							dout(3) <= din_deser;
+						ELSIF (sel = "1001") THEN
+							dout(2) <= din_deser;
+						ELSIF (sel = "1010") THEN
+							dout(1) <= din_deser;
+						ELSIF (sel = "1011") THEN
+							dout(0) <= din_deser;
+						END IF;
+					END IF;
+				END IF;
+			END PROCESS;
 
-		if reset_deser='1' then
-			counter<="0000";
-			linked <= '0';
-		else
-			  if counter=11-bit_depth then
-			     counter<="0000";
-	   else 
-	      if linked = '1' then
-			    counter<=counter+1;
-			else
-			   if counter_bit="01" and counter="0000" then
-					if Dout2(11-bit_depth downto 0)=test_pattern(11-bit_depth downto 0) then
-					    linked <= '1';
-						 
-					else 
-					    linked <= linked;
-					
-					end if;
-					counter <= counter + '1';
-					
-				elsif counter_bit = "11" and counter="0001" then
-				   counter <= counter+2;	
-				
-				else 
-				   counter <= counter + '1';
-				end if;
-	      end if;
-	   end if;
-	end if;
-end if;
-	end process;
-	
-	newcounter_proc: process(counter,clk)
-		begin
-			if rising_edge(clk) then
-				if(counter= "0111") then
-					counter_bit<=counter_bit+1;
-		      end if;
-			end if;
-	 end process;
-				
-bit_depth<= to_integer(unsigned(depth_sel));	
+ 
+ 
+ 
+			clk_out_deser <= NOT counter(2);
+			clk_out_sig <= NOT counter(2);
 
---din_clked<=din_deser when rising_edge(clk);
- --dout<=dout_unclk when rising_edge(clk);
-	
-	sel<=counter;
-   process(clk)
-	begin
-	if(rising_edge(clk)) then
-		if(reset_deser='1') then
-			dout<="000000000000";
-		else
-			if(sel="0000") then dout(11)<=din_deser;
-			elsif(sel="0001") then dout(10)<=(DIN_deser);
-			elsif(sel="0010") then dout(9)<=(DIN_deser);
-			elsif(sel="0011") then dout(8)<=din_deser;
-			elsif(sel="0100") then dout(7)<=din_deser;
-			elsif(sel="0101") then dout(6)<=din_deser;
-		   elsif(sel="0110") then dout(5)<=din_deser;
-			elsif(sel="0111") then dout(4)<=din_deser;
-			elsif(sel="1000") then dout(3)<=din_deser;
-			elsif(sel="1001") then dout(2)<=din_deser;
-			elsif(sel="1010") then dout(1)<=din_deser;
-			elsif(sel="1011") then dout(0)<=din_deser;
-			end if;
-		end if;
-	end if;
-	end process;
-
-				
-				
-	
-clk_out <= not counter(2);
-clk_in  <= not counter(2);
---clk_out<=clk_in;
-      p_clk_out:process(clk_in)
-		begin		
-		if(rising_edge(clk_in)) then
-			if(reset_deser='1') then 
-					dout2<= (others => '0');
-			else 
-				dout2(11-bit_depth downto 0)<=dout(11 downto bit_depth);
-			end if;
-		end if;
-		end process;
-		
-    finalout:process(dout2,linked)
-	 begin
-	 if(linked='0') then 
-		dout_deser<= (others => '0');
-	 else 
-	   dout_deser<=dout2;
-	 end if;
-	 end process;
-	 
-
-
-
-
-end Behavioral;
-
+			p_clk_out : PROCESS (clk_out_sig)
+			BEGIN
+				IF (rising_edge(clk_out_sig)) THEN
+					IF (reset_deser = '1') THEN
+						dout2 <= (OTHERS => '0');
+					ELSE
+						dout2(11 - bit_depth DOWNTO 0) <= dout(11 DOWNTO bit_depth);
+					END IF;
+				END IF;
+			END PROCESS;
+ 
+			finalout : PROCESS (dout2, linked)
+			BEGIN
+				IF (linked = '0') THEN
+					dout_deser <= (OTHERS => '0');
+				ELSE
+					dout_deser <= dout2;
+				END IF;
+			END PROCESS;
+ 
+END Behavioral;
